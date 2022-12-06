628011f054bc36ff5f3e977f740ace3951526c9b
Brendan <brendan.angerman@gmail.com>
2022-12-06 07:00:01 -0700

    day 6 readme

--- src/Day6/Day6.php
+++ src/Day6/Day6.php
@@ -6,6 +6,9 @@ use AOC2022\BaseClass;
 
 class Day6 extends BaseClass
 {
+    public static int $part_1_packet_length = 4;
+    public static int $part_2_packet_length = 14;
+
     public function setupPath() : void
     {
         $this->file_path = __DIR__ . '/input.txt';
@@ -13,17 +16,35 @@ class Day6 extends BaseClass
 
     public function run() : void
     {
-        $this->part1();
-        $this->part2();
+        $this->bothParts();
     }
 
-    public function part1() : void
+    public function bothParts() : void
     {
-        
-    }
+        $signal = $this->data[0];
+        $signal_chars = str_split($signal);
 
-    public function part2() : void
-    {
-        
+        foreach ( $signal_chars as $idx => $char ) {
+
+            if ( $idx > self::$part_1_packet_length
+                && ! $this->part1_answer ) {
+
+                $sub_arr = array_slice($signal_chars, $idx - self::$part_1_packet_length, self::$part_1_packet_length);
+
+                if ( count(array_unique($sub_arr)) === self::$part_1_packet_length ) {
+                    $this->part1_answer = $idx;
+                }
+            }
+
+            if ( $idx > self::$part_2_packet_length
+                && ! $this->part2_answer ) {
+
+                $sub_arr = array_slice($signal_chars, $idx - self::$part_2_packet_length, self::$part_2_packet_length);
+
+                if ( count(array_unique($sub_arr)) === self::$part_2_packet_length ) {
+                    $this->part2_answer = $idx;
+                }
+            }
+        }
     }
 }
